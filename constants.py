7e97b4a2404d28c5b7fc66fd996da978785253ab
Hernan Marano <herchugm@gmail.com>
2025-10-14 18:24:43 -0300

    cambios

--- constants.py
+++ constants.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass
 
 __body__ = "Body"
 __target__ = "AverageScore"
+__title__ = "Title"
 
 @dataclass(frozen=True)
 class DataSetConfig:
     PATH: str = "data/processed/Abstracts.xlsx"
-    FEATURES_BODY = __body__
-    FEATURES_TARGET = __target__
-    COLUMNS = ["Title", __body__, "Scores", __target__]
+    TITLE: str = __title__
+    FEATURES_BODY: str = __body__
+    FEATURES_TARGET: str = __target__
+    COLUMNS = [__title__, __body__, "Topic_1", "Presentation_Type", __target__]
